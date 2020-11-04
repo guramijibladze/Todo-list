fd2ece1d44a5fe2f4df7d7f264d1a073d30d8578
guramijibladze <guramijibladze@gmail.com>
2020-11-04 22:25:54 +0400

    up

---
+++ app.js
@@ -0,0 +1,10 @@
+// selectors
+const todoInput = document.querySelector('.todo-input');
+const todoInput = document.querySelector('.todo-input');
+const todoInput = document.querySelector('.todo-input');
+
+//event listeners
+
+
+
+//functions
