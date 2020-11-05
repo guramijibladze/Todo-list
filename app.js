87510c5a34b27a1917ba885750a3c7e9e9db1611
guramijibladze <guramijibladze@gmail.com>
2020-11-05 18:41:27 +0400

    up

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const todoList = document.querySelector(".todo-list");
 
 //event listeners
 todoButton.addEventListener("click", addTodo);
-
-
+todoList.addEventListener('click', deleteCheck);
 //functions
 
 function addTodo(event){
@@ -16,7 +15,7 @@ function addTodo(event){
     todoDiv.classList.add("todo");
     //Create li
     const newTodo = document.createElement("li");
-    newTodo.innerText = "hey";
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
     //Check mark button
@@ -27,8 +26,14 @@ function addTodo(event){
     //Check trash buttob
     const trashButton = document.createElement('button');
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-    trashButton.classList.add('complete-btn');
+    trashButton.classList.add('trash-btn');
     todoDiv.appendChild(trashButton);
     //append to list
     todoList.appendChild(todoDiv);
+    //Clear Todo Input Value
+    todoInput.value = "";
+}
+
+function deleteCheck(e){
+
 }
